c4bba6e8085b7dcb792ac438c55911af7222aebc
XMADRIGAL <XMADRIGAL@gmail.com>
2025-03-28 16:01:33 +0700

    first commit

--- src/main/java/com/mycourse/Main.java
+++ src/main/java/com/mycourse/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome boss Judens!");
+        System.out.printf("Hello and welcome boss Judens Bandal!");
     }
 }
