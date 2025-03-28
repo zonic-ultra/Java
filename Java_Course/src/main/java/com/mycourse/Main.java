b411ab69a392faaa1fd195938fc40558958daee6
XMADRIGAL <XMADRIGAL@gmail.com>
2025-03-28 16:35:50 +0700

    testing git2

--- Java_Course/src/main/java/com/mycourse/Main.java
+++ Java_Course/src/main/java/com/mycourse/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome Bossss!");
+        System.out.printf("Hello and welcome Bossss Judens!");
     }
 }
